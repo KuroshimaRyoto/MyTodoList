f809bc55434783530f819d21633c824ff3001958
KuroshimaRyoto <kuroshima.ryoto@g.wakayama-u.jp>
2019-07-10 14:48:10 +0900

    Initial Commit

---
+++ MyTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoList
+//
+//  Created by 黒島涼人 on 2019/07/10.
+//  Copyright © 2019 kuroshima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
